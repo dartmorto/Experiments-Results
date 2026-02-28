a180033252354d724f65dbf6f98383c3f098c81b
dart morte <darakozlova543@gmail.com>
2026-02-28 10:02:42 +0300

    classes was finished finally

--- src/model/Result.java
+++ src/model/Result.java
@@ -2,12 +2,43 @@ package model;
 
 import java.time.Instant;
 
-public class Result {
+public final class Result implements Comparable<Result>{
     public  long id;
     public long runId;
     public MeasurementParam param;
     public double value;
     public String unit;
     public String comment;
-    public Instant createdAt;
+    public Instant createdAt = Instant.now();
+
+    public Result(long id, long runId, String comment, double value,
+                  String unit, Instant createdAt, MeasurementParam param) {
+        this.id = id;
+        this.runId = runId;
+        this.param = param;
+        this.comment = comment;
+        this.value = value;
+        this.unit = unit;
+        this.createdAt = Instant.now();
+
+    }
+
+    @Override
+public int compareTo(Result o) {
+    return Long.compare(this.id, o.id);
 }
+
+    @Override
+public String toString() {
+    return "Result{" +
+            "id=" + id +
+            ", runId='" + runId + '\'' +
+            ", comment='" + comment + '\'' +
+            ", param='" + param + '\'' +
+            ", unit='" + unit + '\'' +
+            ", value='" + value + '\'' +
+            ", createdAt=" + createdAt +
+            '}';
+}
+}
+
--- src/model/Run.java
+++ src/model/Run.java
@@ -2,11 +2,39 @@ package model;
 
 import java.time.Instant;
 
-
-public class Run {
+public class Run implements Comparable<Run>{
     public long id;
     public long experimentId;
     public String name;
     public String operator;
     public Instant createdAt;
+
+    public Run(long id, long experimentId, String name,
+               String operator, Instant createdAt){
+
+    this.id = id;
+    this.experimentId = experimentId;
+    this.name = name;
+    this.operator = operator;
+    this.createdAt = Instant.now();}
+
+    @Override
+    public int compareTo(Run o) {
+    return Long.compare(this.id, o.id);
 }
+
+    @Override
+public String toString() {
+    return "Result{" +
+            "id=" + id +
+            ", experimentId='" + experimentId + '\'' +
+            ", name='" + name + '\'' +
+            ", operator='" + operator + '\'' +
+            ", createdAt=" + createdAt +
+            '}';
+}
+}
+
+
+
+
